a704bdb2b638e5cfbac77edc39f70d0c8f1f7ba1
jsee-twilio <ychian@gmail.com>
2020-10-21 22:03:00 +0800

    Command line argument

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const apiKey = require('./appConfig.js')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2]
 
-// const url = 'http://api.weatherstack.com/current?access_key='+ apiKey.weatherStackToken +'&query=Hong%20Kong'
-
-// // console.log(url)
-// request({url: url, json: true}, function (error, response, body) {
-// //   console.log('error:', error); // Print the error if one occurred
-// //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-// //   console.log('body:', body); // Print the HTML for the Google homepage.
-
-// //   const data = JSON.parse(body)
-// //   console.log(body.current)
-// //   console.log(body.current.temperature)
-// //   console.log(body.current.feelslike)
-
-//   console.log(body.current.weather_descriptions[0] + ". It is currently "+ body.current.temperature +" degrees out. It feels like "+ body.current.feelslike + " degrees out")
-// });
-
-
-geocode('muar', (error, data) =>{
-    console.log('Error', error)
-    console.log('Data', data)
-})
-
-forecast('2.05a', 102.57, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+if(!address){
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) =>{
+        if(error){
+            return console.log(error)
+        }
+        forecast(data.latitude, data.longtitude, (error, forecastData) => {
+            if(error){
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,13 +4,13 @@ const request = require('postman-request')
 const forecast = (latitude, longtitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key='+ apiKey.weatherStackToken +'&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longtitude)
 
-    console.log(url)
     request({url: url, json: true}, function (error, response) {
         if (error){
             callback('Unable to connect to weather services!', undefined)
         } else if(response.body.error){
             callback('Unable to find location. Try another search', undefined)
         } else {
+            // console.log(response.body.location.name)
             callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently "+ response.body.current.temperature +" degrees out. It feels like "+ response.body.current.feelslike + " degrees out")
         }
     })
